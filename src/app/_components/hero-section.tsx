bccd810a1808bddf07166035d2917e0cb83ec9f1
UEM CSE (AI & ML) IEDC <iedc.cseaiml@uem.edu.in>
2025-07-26 09:29:38 +0530

    Update hero-section.tsx

--- src/app/_components/hero-section.tsx
+++ src/app/_components/hero-section.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import Image from "next/image";
-import Link from "next/link";
+// import Image from "next/image";
+// import Link from "next/link";
 import Logo from "./logo";
 import CountdownTimer from "./timer";
 
